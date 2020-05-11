003f6e6ebce6904a4e89135865578e52936c6daa
Grisha777 <61342782+Grisha777@users.noreply.github.com>
2020-05-11 23:55:43 +0300

    Create Square.cpp

---
+++ Square.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class square // класс квадрат
+{
+protected:
+    double a;
+public:
+    square(double a) // инициализация поля 
+    {
+        this->a = a;
+    }
+    virtual double Perimeter() // Функция возвращает - периметр
+    {
+        return 4*a;
+    }
+    virtual void print()
+    {
+        cout << "a=" << a <<endl;
+        cout << "perimetr=" << rect->Perimeter() << endl;
+    }
+};
